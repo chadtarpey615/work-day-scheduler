6281aba919241744658b30a13c380713b7887199
Chad Tarpey <chadtarpey@gmail.com>
2020-09-02 16:35:48 -0500

    removed function and added classes with the for loop to change classes to the current, past, or present time

--- logic.js
+++ logic.js
@@ -1,26 +1,33 @@
-
-
 $(document).ready(function () {
+   console.log(moment().format("H"));
 
 
-   let amHours = ["7am", "8am", "9am", "10am", "11am"];
-   let pmHours = ["12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
-   let colorChangeAll = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
-   //let colorChangePm = [12, 13, 14, 15, 16, 17];
+   let amHours = [" 7am", " 8am", " 9am", "10am", "11am", "12pm"];
+   let pmHours = [" 1pm", " 2pm", " 3pm", " 4pm", " 5pm"];
    const date = moment().format("MMM Do YY");
+   var currentTime = moment().format("H");
 
 
-
-
+   
    $("#currentDay").text(moment().format("dddd[,] MMMM Do YYYY"));
 
    for (var i = 0; i < amHours.length; i++) {
       var newRow = $('<div class="row"> </div>').text(amHours[i]);
       $(".container").append(newRow);
       newRow.append($('<div class="col-md-1 time-block"></div>'));
-      newRow.append($('<textarea input= text class="col-md-9 past"></textarea>'));
-      newRow.append($('<button class="col-md-1 saveBtn saveBtn"></button>'));
 
+      if (parseInt(amHours[i]) < currentTime) {
+         newRow.append($('<textarea id="' + parseInt(amHours[i]) + '" input= text class="col-md-9 past"></textarea>'));
+
+      } else if (parseInt(amHours[i]) == currentTime) {
+         newRow.append($('<textarea id="' + parseInt(amHours[i]) + '" input= text class="col-md-9 present"></textarea>'));
+
+      } else {
+         newRow.append($('<textarea id="' + parseInt(amHours[i]) + '" input= text class="col-md-9future"></textarea>'));
+
+      }
+      newRow.append($('<button class="col-md-1 saveBtn saveBtn fas fa-clipboard"></button>'));
+      console.log(parseInt(amHours[i]));
 
 
    };
@@ -29,76 +36,60 @@ $(document).ready(function () {
       var nextLine = $('<div class="row"> </div>').text(pmHours[i]);
       $(".container").append(nextLine);
       nextLine.append($('<div class="col-md-1 time-block"></div>'));
-      nextLine.append($('<textarea input= text class="col-md-9 past" ></textarea>'));
 
-      nextLine.append($('<button class="col-md-1 saveBtn saveBtn"></button>'));
+      if ((parseInt(pmHours[i]) + 12) < currentTime) {
+         nextLine.append($('<textarea id="' + (parseInt(pmHours[i]) + 12) + '" input= text class="col-md-9 past"></textarea>'));
 
+      } else if ((parseInt(pmHours[i]) + 12) == currentTime) {
+         nextLine.append($('<textarea id="' + (parseInt(pmHours[i]) + 12) + '" input= text class="col-md-9 present"></textarea>'));
+
+      } else {
+         nextLine.append($('<textarea id="' + (parseInt(pmHours[i]) + 12) + '" input= text class="col-md-9 future"></textarea>'));
+
+      }
+      nextLine.append($('<button class="col-md-1 saveBtn saveBtn fas fa-clipboard"></button>'));
+      //classColor();
+
+     
+   
 
    };
-   function classColor() {
-      for (var i = 0; i < colorChangeAll.length; i++) {
-         var currentTime = moment().format("H");
+   // tried to do it with a function //
 
-         if (parseInt(colorChangeAll[i] < currentTime)) {
-            $("textarea").removeClass("future");
-            $("textarea").removeClass("present");
-            $("textarea").addClass("past")
-
-
-         } else if (parseInt(colorChangeAll[i] > currentTime)) {
-            $("textarea").removeClass("past");
-            $("textarea").removeClass("present");
-            $("textarea").addClass("future")
+   //function classColor(hours, row) {
+   //for (var i = 0; i < colorChangeAll.length; i++) {
+   // if (parseInt(hours < currentTime)) {
+   //    $("textarea").removeClass("future");
+   //    $("textarea").removeClass("present");
+   //    $("textarea").addClass("past")
 
 
-         } else if (parseInt(colorChangeAll[i] === currentTime)) {
-            $("textarea").removeClass("future");
-            $("textarea").removeClass("past");
-            $("textarea").addClass("past")
-         }
-      }
+   // } else if (parseInt(hours > currentTime)) {
+   //    $("textarea").removeClass("past");
+   //    $("textarea").removeClass("present");
+   //    $("textarea").addClass("future")
 
 
-
-      $("button").on("click", function () {
-         var timeOfday = $(this).parent().attr("id");
-         var textContent = $("textarea").val().trim();
-
-         localStorage.setItem(timeOfday, textContent);
-         console.log(timeOfday, textContent);
-      });
-      console.log(moment().format("H"));
-   }
+   // } else if (parseInt(hours === currentTime)) {
+   //    $("textarea").removeClass("future");
+   //    $("textarea").removeClass("past");
+   //    $("textarea").addClass("past")
+   // }
+   //}
 
 
+   // save to local storage
+   $("button").on("click", function () {
+      var timeOfday = $(this).prev().attr("#");
+      var text = $(this).prev().val();
 
-
-
-
-   // function for chaging event colors past present future
-
-
-   //$("button").click(timeColorsAm());
-
+      localStorage.setItem(timeOfday, text);
+      console.log(timeOfday, text);
+   });
 
 
 
 });
 
 
-
-
-
-
-
-
-
-
-// loop for the am hours and then a loop for the pm hours
-
-
-
-
-
-// Parse into localStorage
 
